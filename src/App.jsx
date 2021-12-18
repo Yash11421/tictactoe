f145af6f006b238e1f46b2406bc563c9df2ae826
YASH <yashbhanushali142@gmail.com>
2021-12-18 19:42:06 +0530

    Init Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal .</p>
+      <small>Hello this is </small>
+    </>
+  );
+};
+
+export default App;
